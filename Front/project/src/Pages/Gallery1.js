e16ee4c125886b8c5521a003f8b9a06f444ec4ff
Benjamin <144312142+Bnjmn-P@users.noreply.github.com>
2024-02-19 16:45:37 +1300

    Update Gallery1.js
    Fixed the issue of the images not loading. Imported them to the page instead of just linking them.

--- Front/project/src/Pages/Gallery1.js
+++ Front/project/src/Pages/Gallery1.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./Gallery1.css";
+import Tame1 from "../Images/Tame1.jpg";
+import Tame2 from "../Images/Tame2.jpg";
 
 const Gallery1 = () => {
   return (
     <div className="gallery-container">
       <h1>Gallery 1</h1>
       <div className="photo-gallery">
-        <div className="gallery-item" data-title="Tame 1" data-caption="Caption for Image 1">
-          <img src={require("../Images/Tame1.jpg").default} alt="Tame1" />
+        <div className="gallery-item" data-title="Tame 1" data-caption="Picture of a handsome cat">
+          <img src={Tame1} alt="Tame 1" />
         </div>
-        <div className="gallery-item" data-title="Tame 2" data-caption="Caption for Image 2">
-          <img src={require("../Images/Tame2.jpg").default} alt="Tame2" />
+        <div className="gallery-item" data-title="Tame 2" data-caption="Another picture of a handsome cat">
+          <img src={Tame2} alt="Tame 2" />
         </div>
         {/* Add more gallery items as needed */}
       </div>
